0eda143b53657a2599df73ee4a0b03b15bb85325
Sayed Abdel Badie <34669926+psydology@users.noreply.github.com>
2021-03-19 21:53:11 +0200

    landing page test

---
+++ README.md
@@ -0,0 +1,38 @@
+# Landing Page Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+ # starting code
+ first I declared a global variable 
+
+ # helper functions
+ there are three helper functions as below:
+ * 1- isInViewport function it return true or false so it help me to know if section is in viewport or not
+ * 2- removeActivelink function it help me to reset any change happened in navbar
+ * 3- hideArrow function it help me to hide arrow if page scroll at the top
+
+
+ # main function
+
+there are 8 main functions as below:
+* addNavBar to add navbar
+* addActiveSection to make section active
+* activeLink to make link active
+* returnToUP to scroll back to the top of the page
+* collapseSctions to collapse a section if clicked 
+* hideFixed to hide navbar if page stopped scroll move
+* addPerformanceTime to measure time which site takes to load
+* doResponsive to make site responsive
+
+# some change in html and css
+by adding section s in html and also add fontawesome libalary link & icon
+add some style in media query to make site responsive
---
+++ js/app.js
@@ -0,0 +1,205 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+const startingLandingPage = performance.now();
+const ul = document.getElementById('navbar__list');
+const sections = document.querySelectorAll('section');
+let fragment = document.createDocumentFragment();
+const arrowup = document.getElementById('scrollToTop');
+const heading = document.querySelectorAll('h2');
+let navbar = document.querySelector('.page__header');
+const perfTime = document.querySelector('.page__footer');
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function removeActivelink(){
+    const a = document.querySelectorAll('.menu__link');
+    a.forEach(function (link) {
+        link.classList.remove("active");
+    })
+}
+
+function hideArrow(){
+    if (window.scrollY < 450 ){
+        arrowup.style.visibility = "hidden";
+    }else{
+        arrowup.style.visibility = "visible";
+    }
+}
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav 
+
+function addNavBar(){
+    
+    sections.forEach(function(section){
+        let li = document.createElement("li"); 
+        let nav = section.getAttribute("data-nav")
+        let a = document.createElement("a");
+        let textNode = document.createTextNode(nav);
+        // Scroll to section on link click
+        a.addEventListener("click" , function(e){
+         e.preventDefault();
+        section.scrollIntoView({behavior:"smooth"})
+    });
+    a.className = "menu__link";
+    a.setAttribute("href" , "#")
+    a.appendChild(textNode)
+    
+    li.appendChild(a);
+    fragment.appendChild(li);
+    
+    })
+   
+    ul.appendChild(fragment);
+    
+}
+
+
+
+
+
+// Add class 'active' to section when near top of viewport
+function addActiveSection(){
+    removeActivelink();
+    sections.forEach (function(section){  
+        if (isInViewport(section)){
+             section.classList.add("your-active-class"); 
+            activeLink(section);
+        } else {
+            section.classList.remove("your-active-class");
+        } 
+    })
+        
+
+    function activeLink(section) {
+        const a = document.querySelectorAll('.menu__link');
+        a.forEach(function (link) {
+            if (section.getAttribute('data-nav') == link.textContent) {
+                link.classList.add("active");
+
+            } else {
+                link.classList.remove("active");
+            }
+        });
+    }
+}
+
+
+// Scroll to anchor ID using scrollTO event
+function returnToUP(){
+    arrowup.addEventListener('click' , function(e){
+        e.preventDefault();
+        window.scrollTo(0,0,"smooth");
+        removeActivelink();
+    })
+}
+
+returnToUP();
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+addNavBar();
+window.addEventListener("scroll" , hideArrow);
+
+
+
+// Set sections as active
+
+window.addEventListener("scroll" ,addActiveSection);
+
+// make sections collapse
+function collapseSctions(){
+    heading.forEach(function (h){
+        h.style = ("background-color : rgba(0,0,0,.1); cursor: pointer;");
+        h.addEventListener("click" , function(){
+            this.parentNode.classList.toggle("collapse");
+            })
+    })
+}
+
+collapseSctions();
+
+
+//Hide fixed navigation bar while not scrolling
+function hideFixed(){
+    let timer = null;
+    window.addEventListener('scroll', function() {
+        if(timer !== null) {
+            clearTimeout(timer);
+            navbar.style = "position:fixed;";       
+        }
+        timer = setTimeout(function() {
+            navbar.style = "position:relative;";
+        }, 5000);
+    });
+}
+hideFixed();
+
+const endingLandingPage = performance.now();
+ // add performance time 
+
+ function addPerformanceTime(){
+     let p = document.createElement('p');
+     p.innerHTML = "this site loaded on " + (endingLandingPage - startingLandingPage) + " ms";
+     perfTime.appendChild(p);
+
+ }
+
+ addPerformanceTime();
+console.log("this site loaded on " + (endingLandingPage - startingLandingPage) + " ms")
+
+// add class to responsive view
+
+function doResponsive() {
+    var x = document.getElementById("navbar__list");
+    if (x.className === "navbar__list") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar__list";
+    }
+  }
+
